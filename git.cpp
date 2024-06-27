706980e1f457801cd51687034d5a5d42de6b2189
Bishal Kumar <vishalkumar7970751360@gmail.com>
2024-06-27 08:55:54 +0530

    add new language

---
+++ git.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,r;
+    cout<<"enter a:";
+    cin>>a;
+    cout<<"enter B:";
+    cin>>b;
+    r=a+b;
+    cout<<"sum is:"<<r;
+}
